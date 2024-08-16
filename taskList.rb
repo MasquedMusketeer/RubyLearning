622f6c2373e17e2ab85fbe031f9851d3dac5dcb2
MasquedMusketeer <andredfkingofgames@gmail.com>
2024-08-16 16:48:39 -0300

    Update taskList.rb
    end of second project

--- taskList.rb
+++ taskList.rb
@@ -2,10 +2,6 @@ taskList = []
 taskStatus = []
 taskMenu = ["---Ruby Task List---", '1 - Add Task', "2 - Delete Task", "3 - View rogress", "0 - Close App"]
 
-def numCorrection(taskList,taskStatus,taskNum)
-
-end
-
 def showMenu(menuString)
   menuString.each do |string|
     puts string
@@ -15,21 +11,31 @@ end
 def addTask(taskList,taskStatus)
   puts "Type the task to be added:"
   taskString = gets.chomp
-  taskBuffer = ""
-  taskNum = taskMenu.size + 1
-  taskBuffer << (taskNum + " . " + taskString)
-  taskList << taskBuffer
+  taskList << taskString
   taskStatus << "In The Making"
 end
 
-def deleteTask(taskNum,taskList,taskStatus)
+def deleteTask(taskList,taskStatus)
+  puts "Type the number of the task to be deleted:"
+  taskNum = (gets.chomp).to_i
   taskList.each do |string|
-    if string.include?(taskNum)
       taskList.delete_at(taskNum - 1)
       taskStatus.delete_at(taskNum - 1)
-      numCorrection(taskList,taskStatus,taskNum)
+  end
+  puts "Entry #{taskNum} was deleted from queue."
+end
+
+taskCounter = 0
+def showTask(taskList,taskStatus,taskCounter)
+  fullTask = ""
+  if taskList.empty?
+    puts "List is empty"
+  else
+    taskList.zip(taskStatus) do |string,stats|
+        taskCounter += 1
+        fullTask << (taskCounter.to_s + " . " + string + " -> " + stats)
+        puts fullTask
     end
-    puts "Entry #{taskNum} was deleted from queue."
   end
 end
 
@@ -37,6 +43,15 @@ loop do
   showMenu(taskMenu)
   input = gets.chomp
   case input
-  when 1
+  when "1"
     addTask(taskList,taskStatus)
+  when "2"
+    deleteTask(taskList,taskStatus)
+  when "3"
+    showTask(taskList,taskStatus,taskCounter)
+  when "0"
+    break
+  else
+    puts "Invalid Option"
+  end
 end
