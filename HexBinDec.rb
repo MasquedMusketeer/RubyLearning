a7fe59026c618cabcfbc96bdc4b1cbb331381b18
MasquedMusketeer <andredfkingofgames@gmail.com>
2024-08-19 08:51:45 -0300

    Update HexBinDec.rb
    basic documentation added

--- HexBinDec.rb
+++ HexBinDec.rb
@@ -1,9 +1,9 @@
-def userInput()
+def userInput()                       #getting user input
   usr = (gets.chomp).to_s
   return usr
 end
 
-def direct(value,flag)
+def direct(value,flag)                #conversion selector
   case flag
   when "d"
     decimal(value)
@@ -14,29 +14,30 @@ def direct(value,flag)
   end
 end
 
+#_____________Convert any non-decimal value to decimal________________
 def valueConver(value)
   hexTable = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
   hexRange = ("A".."F").to_a
   decRange = ("2".."9").to_a
   conversionFlag = ""
   fullValue = 0
-  valueUp = value.upcase
+  valueUp = value.upcase                #make all characters uppercase to make the conversion uniform
+  valueRev = valueUp.reverse            #invert number for processing
 
   valueUp.each_char do |digit|
-    if hexRange.include?(digit)
+    if hexRange.include?(digit)         #check if there is any letter in the value provided to determine if its hexadecimal
       conversionFlag = "hex"
       break
-    elsif decRange.include?(digit)
-      conversionFlag = "dec"
+    elsif decRange.include?(digit)      #check if there is any decimal digit in the value provided
+      conversionFlag = ""
     else
       conversionFlag = "bin"
     end
   end
 
   case conversionFlag
-  when "hex"
+  when "hex"                           #conversion hex digit by digit (it uses the index of a lookup table to determine by whitch number the 16^n must be multiplied)
     digitCounter = 0
-    valueRev = valueUp.reverse
     valueRev.each_char do |digit|
       digitValue = hexTable.index(digit).to_i
       fullDigitValue = digitValue * (16**digitCounter)
@@ -44,9 +45,8 @@ def valueConver(value)
       digitCounter += 1
     end
     return fullValue
-  when "bin"
+  when "bin"                          #conversion bin digit by digit
     digitCounter = 0
-    valueRev = valueUp.reverse
     valueRev.each_char do |digit|
       fullDigitValue = digit.to_i * (2** digitCounter)
       fullValue += fullDigitValue
